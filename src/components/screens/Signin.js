6af92ab01a79716d703eff1e0b57612c1ffe5d3d
saiaakaash <sai130103@gmail.com>
2022-07-19 22:59:50 +0530

    commit 7client

--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link, useHistory} from "react-router-dom"
+import M from 'materialize-css';
 
 const Signin = ()=>{
   const history = useHistory();
-  const [password, setPasword] = useState("")
+  const [password, setPassword] = useState("")
   const [email, setEmail] = useState("") 
   const PostData = ()=>{
       if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
           M.toast({html: "Invalid Email", classes:"#c62828 red darken-3"})
           return
       }
-      fetch("http://localhost:5000/signup",{
+      fetch("/signin",{
           method:"post",
           headers: {
            "Content-Type":"application/json"
@@ -21,10 +22,13 @@ const Signin = ()=>{
           })
       }).then(res=>res.json())
       .then(data=>{
+          console.log(data)
           if(data.error){
               M.toast({html: data.error, classes:"#c62828 red darken-3"})
           }
           else{
+              localStorage.setItem("jwt", data.token)
+              localStorage.setItem("user", JSON.stringify(data.user))
               M.toast({html: "Signed in Successfully", classes:"#43a047 green darken-1"})
               history.push('/')
           }
@@ -52,7 +56,9 @@ const Signin = ()=>{
         value={password}
         onChange={(e)=>setPassword(e.target.value)} 
         />
-        <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+        <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+        onClick={ ()=>PostData()}
+        >
             Login
         </button>
         <h5>
